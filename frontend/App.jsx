32467ead9e9b741bdd4d24daabf4215266ffc0ef
SuchitKS <155285799+SuchitKS@users.noreply.github.com>
2025-10-06 14:20:43 +0530

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -5,9 +5,8 @@ import Events from './Events'
 import EventForm from './EventForm'
 import Organisers from './Organisers'
 import Participants from './Participants'
-import Volunteers from './Volunteers'
 import RegisterEvent from './RegisterEvent'
-import VolunteerEvents from './VolunteerEvents'
+import Volunteers from './VolunteerEvents'
 
 function App() {
   return (
@@ -19,9 +18,9 @@ function App() {
       <Route path="/participants.html" element={<Participants />} />
       <Route path="/volunteers.html" element={<Volunteers />} />
       <Route path="/registerevent.html" element={<RegisterEvent />} />
-      <Route path="/volunteer_events.html" element={<VolunteerEvents />} />
     </Routes>
   )
 }
 
 export default App
+
