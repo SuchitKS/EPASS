ecea8e26068e0d614c47bd2a2c4d2f28e638b65f
SuchitKS <155285799+SuchitKS@users.noreply.github.com>
2025-10-08 00:13:49 +0530

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Events from './Events'
 import EventForm from './EventForm'
@@ -8,37 +8,19 @@ import Participants from './Participants'
 import Volunteers from './Volunteers'
 import RegisterEvent from './RegisterEvent'
 import VolunteerEvents from './VolunteerEvents'
-import './style.css'
 
 function App() {
-  const location = useLocation()
-  
-  // Add page-specific class to body based on route
-  React.useEffect(() => {
-    const path = location.pathname
-    document.body.className = path === '/' ? 'login-page' : ''
-    
-    // Cleanup function to remove class when component unmounts
-    return () => {
-      document.body.className = ''
-    }
-  }, [location])
-
   return (
-    <div className="app-container">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/event_form" element={<EventForm />} />
-        <Route path="/organisers" element={<Organisers />} />
-        <Route path="/participants" element={<Participants />} />
-        <Route path="/volunteers" element={<Volunteers />} />
-        <Route path="/registerevent" element={<RegisterEvent />} />
-        <Route path="/volunteer_events" element={<VolunteerEvents />} />
-        {/* Add a catch-all route for 404 pages */}
-        <Route path="*" element={<div>Page not found</div>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/event_form" element={<EventForm />} />
+      <Route path="/organisers" element={<Organisers />} />
+      <Route path="/participants" element={<Participants />} />
+      <Route path="/volunteers" element={<Volunteers />} />
+      <Route path="/registerevent" element={<RegisterEvent />} />
+      <Route path="/volunteer_events" element={<VolunteerEvents />} />
+    </Routes>
   )
 }
 
